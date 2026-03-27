1fdb07be9e470564ba18020d6784026310fbf411
Lubeli <144096691+Lubeli@users.noreply.github.com>
2026-03-27 09:25:27 +0900

    Update 1-3-2.c

---
+++ code/1-3/1-3-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "util1_3.h"
+
+void logger_global(char*message){
+    static int counter = 0;
+    printf("log %2d: %s\n", counter++, message);
+}
+
+void task2(){
+    for(int i = 0; i< 5; i++){
+        logger_static_inline("task2");
+        logger_global("task2");
+    }   
+}
