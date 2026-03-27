21bac9a7cc7db79d30d1301bd9dd19f4ed47aa9a
Lubeli <144096691+Lubeli@users.noreply.github.com>
2026-03-27 16:51:47 +0900

    Update 4-3.c

--- code/4-3/4-3.c
+++ code/4-3/4-3.c
@@ -11,8 +11,8 @@ int main(){
         printf("address of arr[%d]: %p\n", i, addr_0);
         printf("address of arr[%d+1]: %p\n", i, addr_1);
         
-        printf("&(arr[%d] - &(arr[%d+1]) = %d\n", i, i, (long long)addr_1 
-        - (long long)addr_0);
+        printf("&(arr[%d] - &(arr[%d+1]) = .*\n", i, i, (unsigned long long)addr_1 
+        - (unsigned long long)addr_0);
         printf("\n");
     }
     return 0;
